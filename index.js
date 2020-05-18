43438c78b838d617932110f0e613c644d504f389
emnh <hvidevold@gmail.com>
2020-05-18 18:08:04 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-// Import stylesheets
-import './style.css';
+import * as PIXI from 'pixi.js';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+function append(program, text) {
+  return program + text + '\n';
+}
+
+function statement(program, text) {
+  return program + text + ';\n';
+}
+
+let program = "";
+program = append(program, function() {
+"new function main",
+"entrypoint main",
+"in main define const app",
+"let app be new PIXI application",
+"add arg object",
+"let width be 800",
+"let height be 600",
+"let backgroundColor be 0x1099bb",
+"let resolution be window devicePixelRatio"
+});
